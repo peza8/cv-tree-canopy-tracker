83c17a25b031698b817188ab0698a58d6f81cc82
Josh Perry <josh.perry@roostermornings.com>
2020-10-25 23:31:54 +0200

    chore(open-cv): fixed include paths

--- zenn/image-processing/zenn_img_annotation.cpp
+++ zenn/image-processing/zenn_img_annotation.cpp
@@ -3,20 +3,20 @@
 #include <stdexcept>
 
 #include <spdlog.h>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
-// #include <opencv2/core.hpp>
-// #include <opencv2/imgcodecs.hpp>
-// #include <opencv2/highgui.hpp>
+// Look at this: https://github.com/opencv/opencv/blob/master/samples/cpp/example_cmake/CMakeLists.txt
 
 namespace zenn {
 
 void DrawCenterCircleOnImg(const std::string& img_path) {
-  // cv::Mat aerial_img = cv::imread(img_path, cv::IMREAD_COLOR);
-  // if (aerial_img.empty()) {
-  //   const std::string err_msg = "Could not find image: " + img_path; 
-  //   throw std::runtime_error(err_msg);
-  // }
+  cv::Mat aerial_img = cv::imread(img_path, cv::IMREAD_COLOR);
+  if (aerial_img.empty()) {
+    const std::string err_msg = "Could not find image: " + img_path; 
+    throw std::runtime_error(err_msg);
+  }
   // cv::imshow(aerial_img);
 }
 
